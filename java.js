1aec06419b0e6464670a860ce431739819527f6b
ملك أحمد -77 <100773772+Malak-77@users.noreply.github.com>
2022-03-18 13:20:48 +0300

    c5-cw3

--- java.js
+++ java.js
@@ -4,3 +4,20 @@ let budget = 20;
 console.log(budget>30&&budget<80);
  budget+=30;
  console.log(budget>30&&budget<80);
+let grade= prompt("ادخل درجتك ")
+
+if(grade>=90&&grade<100){
+    console.log("لقد حصلت على امتياز ")
+} else if (grade>=80){
+    console.log("لقد حصلت على جيد جدا ")
+} else if (grade>=70){
+    console.log("لقد حصلت على جيد ")
+} else if (grade>=60){
+    console.log("لقد حصلت مقبول ")
+} else if (grade>=50){
+    console.log("لقد حصلت على مقبول ")
+} else {
+    console.log("راسب ")
+}
+
+
